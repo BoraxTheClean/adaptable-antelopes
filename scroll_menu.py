338d96dfd278bc8cd9d3e52f5690c56fa062750a
George <mazzeogeorge@gmail.com>
2021-07-11 08:44:08 -0500

    scroll_menu flake8 fixes

--- scroll_menu.py
+++ scroll_menu.py
@@ -1,26 +1,20 @@
-#!/usr/bin/env python
-"""
-A simple example of a scrollable pane.
-"""
+"""A simple example of a scrollable pane."""
 from prompt_toolkit.application import Application
 from prompt_toolkit.application.current import get_app
-from prompt_toolkit.completion import WordCompleter
+# from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.key_binding import KeyBindings
-from prompt_toolkit.key_binding.bindings.focus import focus_next, focus_previous
+from prompt_toolkit.key_binding.bindings.focus import (
+    focus_next, focus_previous
+)
 from prompt_toolkit.layout import (
-    CompletionsMenu,
-    Float,
-    FloatContainer,
-    HSplit,
-    Layout,
-    ScrollablePane,
-    VSplit,
+    CompletionsMenu, Float, FloatContainer, HSplit, Layout, ScrollablePane,
+    VSplit
 )
 from prompt_toolkit.widgets import Frame, Label, TextArea
 
 
-def main():
-    # Create a big layout of many text areas, then wrap them in a `ScrollablePane`.
+def main() -> None:
+    """Create a big layout of many text areas, then wrap them in a `ScrollablePane`."""
     root_container = VSplit(
         [
             Label("<left column>"),
@@ -34,7 +28,7 @@ def main():
                                     Frame(
                                         TextArea(
                                             text=f"label-{i}",
-                                            completer=animal_completer,
+                                            # completer=animal_completer,
                                         )
                                     )
                                     for i in range(20)
@@ -64,7 +58,7 @@ def main():
     kb = KeyBindings()
 
     @kb.add("c-c")
-    def exit(event) -> None:
+    def exit_(event: str) -> None:
         get_app().exit()
 
     kb.add("tab")(focus_next)
@@ -77,43 +71,43 @@ def main():
     application.run()
 
 
-animal_completer = WordCompleter(
-    [
-        "alligator",
-        "ant",
-        "ape",
-        "bat",
-        "bear",
-        "beaver",
-        "bee",
-        "bison",
-        "butterfly",
-        "cat",
-        "chicken",
-        "crocodile",
-        "dinosaur",
-        "dog",
-        "dolphin",
-        "dove",
-        "duck",
-        "eagle",
-        "elephant",
-        "fish",
-        "goat",
-        "gorilla",
-        "kangaroo",
-        "leopard",
-        "lion",
-        "mouse",
-        "rabbit",
-        "rat",
-        "snake",
-        "spider",
-        "turkey",
-        "turtle",
-    ],
-    ignore_case=True,
-)
+# animal_completer = WordCompleter(
+#     [
+#         "alligator",
+#         "ant",
+#         "ape",
+#         "bat",
+#         "bear",
+#         "beaver",
+#         "bee",
+#         "bison",
+#         "butterfly",
+#         "cat",
+#         "chicken",
+#         "crocodile",
+#         "dinosaur",
+#         "dog",
+#         "dolphin",
+#         "dove",
+#         "duck",
+#         "eagle",
+#         "elephant",
+#         "fish",
+#         "goat",
+#         "gorilla",
+#         "kangaroo",
+#         "leopard",
+#         "lion",
+#         "mouse",
+#         "rabbit",
+#         "rat",
+#         "snake",
+#         "spider",
+#         "turkey",
+#         "turtle",
+#     ],
+#     ignore_case=True,
+# )
 
 
 if __name__ == "__main__":
