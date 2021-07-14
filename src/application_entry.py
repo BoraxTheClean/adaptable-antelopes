f59f5df55ae989c7fde45d0ff74612a9b059c0cd
Khiem Nguyen <Ppkhiemnguyen@gmail.com>
2021-07-14 10:22:27 -0700

    Refactor All the Things 🧹 😄 ❗  (#48)
    * Decouple dialogs into one folder
    
    * Move application's states to `application/`
    
    * :shirt: Refactor Thoughtbox into classes for better management
    
    * Initialize ApplicationState as an object to be passed around.
    
    * Fixed type annotation
    
    * Improve docstring for message.py
    
    * Decouple editor.py
    
    Move `MenuNav` into `navigation/`
    Move `editor.py` to `application/`
    
    * Remove old, unused code
    
    * Update type annotation.

--- src/application_entry.py
+++ src/application_entry.py
@@ -1,9 +1,11 @@
-from text.text_editor import run
+from application.editor import ThoughtBox
+
+TB = ThoughtBox()
 
 
 def main() -> None:
     """Start the text editor application."""
-    run()
+    TB.run()
 
 
 if __name__ == "__main__":
