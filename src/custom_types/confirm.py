e4378cd4234b12aa298c8642a0064937d6a14399
Jason Luo <31069084+SharpBit@users.noreply.github.com>
2021-07-15 09:28:43 -0400

    fix save as bug (#56)
    - if the existing file will be overriden, show a confirm prompt

---
+++ src/custom_types/confirm.py
@@ -0,0 +1,36 @@
+from asyncio import Future
+
+from prompt_toolkit.layout.containers import HSplit
+from prompt_toolkit.layout.dimension import D
+from prompt_toolkit.widgets import Button, Dialog, Label
+
+from custom_types.ui_types import PopUpDialog
+
+
+class ConfirmDialog(PopUpDialog):
+    """Dialog box to confirm or cancel"""
+
+    def __init__(self, title: str, text: str):
+        self.future = Future()
+
+        def set_done() -> None:
+            """Confirm the dialog."""
+            self.future.set_result(True)
+
+        def set_cancel() -> None:
+            """Cancel the dialog."""
+            self.future.set_result(False)
+
+        yes_button = Button(text="Yes", handler=(lambda: set_done()))
+        no_button = Button(text="No", handler=(lambda: set_cancel()))
+
+        self.dialog = Dialog(
+            title=title,
+            body=HSplit([Label(text=text)]),
+            buttons=[yes_button, no_button],
+            width=D(preferred=80),
+            modal=True,
+        )
+
+    def __pt_container__(self):
+        return self.dialog
