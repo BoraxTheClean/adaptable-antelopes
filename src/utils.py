204b03b2c9b126e623abd15fe0e48c87831861b3
Owen Collier-Ridge <owencollierridge@curative.com>
2021-07-16 22:46:15 -0400

    Add a utility function to find a unique filename in the .thought_box directory

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,26 @@
+import os
+
 from constants import NOTES_DIR
 
 
 def display_path(path: str) -> str:
     """Replaces NOTES_DIR with 'Explorer'"""
     return path.replace(NOTES_DIR, "Explorer")
+
+
+def get_unique_filename(path: str) -> str:
+    """Get a unique filename for a given path."""
+    map_of_all_filenames = {i: True for i in os.listdir(path)}
+    default_filename = "Thought Box Note"
+    if default_filename not in map_of_all_filenames:
+        return default_filename
+
+    suffix = 1
+
+    candidate_file_name = default_filename + " " + str(suffix)
+
+    while candidate_file_name in map_of_all_filenames:
+        suffix += 1
+        candidate_file_name = default_filename + " " + str(suffix)
+
+    return candidate_file_name
