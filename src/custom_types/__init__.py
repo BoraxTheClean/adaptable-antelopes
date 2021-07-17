9aa01b1a53cf2683e51c87482756e5fac8c6e23d
standard-affect <86736798+standard-affect@users.noreply.github.com>
2021-07-17 12:28:24 -0400

    Warn exit unsaved (#79)

--- src/custom_types/__init__.py
+++ src/custom_types/__init__.py
@@ -1,7 +1,15 @@
 from .confirm import ConfirmDialog
 from .message import MessageDialog
+from .save_exit import SaveExitDialog
 from .scroll_menu import ScrollMenuDialog
 from .text_input import TextInputDialog
 from .ui_types import PopUpDialog
 
-__all__ = [TextInputDialog, ScrollMenuDialog, MessageDialog, ConfirmDialog, PopUpDialog]
+__all__ = [
+    TextInputDialog,
+    ScrollMenuDialog,
+    MessageDialog,
+    ConfirmDialog,
+    SaveExitDialog,
+    PopUpDialog,
+]
---
+++ src/custom_types/save_exit.py
@@ -0,0 +1,41 @@
+from asyncio import Future
+
+from prompt_toolkit.layout.containers import HSplit
+from prompt_toolkit.layout.dimension import D
+from prompt_toolkit.widgets import Button, Dialog, Label
+
+from constants import DIALOG_WIDTH
+from custom_types.ui_types import PopUpDialog
+
+
+class SaveExitDialog(PopUpDialog):
+    """Dialog box to offer option to save and exit, exit without saving, or cancel"""
+
+    def __init__(self, title: str, text: str):
+        self.future = Future()
+
+        def set_save_exit() -> None:
+            """Save file and exit"""
+            self.future.set_result("save")
+
+        def set_nosave_exit() -> None:
+            """Exit without saving file"""
+            self.future.set_result("nosave")
+
+        def set_cancel() -> None:
+            """Cancel dialog"""
+            self.future.set_result("cancel")
+
+        save_exit_button = Button(text="Yes", handler=set_save_exit)
+        nosave_exit_button = Button(text="No", handler=set_nosave_exit)
+        cancel_button = Button(text="Cancel", handler=set_cancel)
+        self.dialog = Dialog(
+            title=title,
+            body=HSplit([Label(text=text)]),
+            buttons=[save_exit_button, nosave_exit_button, cancel_button],
+            width=D(preferred=DIALOG_WIDTH),
+            modal=True,
+        )
+
+    def __pt_container__(self):
+        return self.dialog
